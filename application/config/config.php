7aed60a3d0e31b53ac9b496c21d485b9647c6a0d
achoos007 <biju.iitm@gmail.com>
2013-07-25 23:27:16 -0700

    url changed

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/online-latest/trunk/';
+$config['base_url']	= 'http://localhost/onlinelatest/trunk/';
 
 /*
 |--------------------------------------------------------------------------
